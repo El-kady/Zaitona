1a622b8b657c153f6bde1551933a4f9da1a610d7
mhesham90 <mohammedhesham.1990@gmail.com>
2017-03-12 15:48:26 +0200

    added export table for comments+sections+categories+materials

--- _install/categories.sql
+++ _install/categories.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 07, 2017 at 07:40 PM
+-- Generation Time: Mar 12, 2017 at 03:46 PM
 -- Server version: 5.7.17-0ubuntu0.16.10.1
 -- PHP Version: 7.0.13-0ubuntu0.16.10.1
 
@@ -30,20 +30,25 @@ CREATE TABLE `categories` (
   `id` int(11) NOT NULL,
   `parent_id` int(11) NOT NULL DEFAULT '0',
   `title` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(2550) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `pic` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `user_id` int(11) NOT NULL
+  `user_id` int(11) NOT NULL DEFAULT '1'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `categories`
 --
 
-INSERT INTO `categories` (`id`, `parent_id`, `title`, `created_at`, `user_id`) VALUES
-(1, 0, 'dev', '2017-03-05 19:16:51', 1),
-(2, 0, 'adminstration', '2017-03-05 19:16:51', 0),
-(5, 1, 'Server Side', '2017-03-07 15:14:56', 1),
-(6, 1, 'Client Side', '2017-03-07 16:04:21', 1),
-(7, 2, 'Adminstration', '2017-03-07 16:04:21', 1);
+INSERT INTO `categories` (`id`, `parent_id`, `title`, `description`, `pic`, `created_at`, `user_id`) VALUES
+(1, 0, 'dev', '', '', '2017-03-05 19:16:51', 1),
+(2, 0, 'adminstration', '', '', '2017-03-05 19:16:51', 0),
+(5, 1, 'Server Side', '', '', '2017-03-07 15:14:56', 1),
+(6, 1, 'Client Side', '', '', '2017-03-07 16:04:21', 1),
+(7, 2, 'RedHat Adminstration', '', '', '2017-03-07 16:04:21', 1),
+(8, 0, 'Network', '', '', '2017-03-08 21:51:36', 1),
+(9, 8, 'Network Introduction', '', '', '2017-03-08 21:52:06', 1),
+(10, 2, 'Shell Script', '', '', '2017-03-12 04:52:27', 1);
 
 --
 -- Indexes for dumped tables
@@ -63,7 +68,7 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- _install/courses.sql
+++ _install/courses.sql
@@ -1,14 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.0
--- http://www.phpmyadmin.net
+-- version 4.6.4deb1
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Mar 10, 2017 at 04:11 PM
--- Server version: 5.5.54-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.21
+-- Host: localhost:3306
+-- Generation Time: Mar 12, 2017 at 03:45 PM
+-- Server version: 5.7.17-0ubuntu0.16.10.1
+-- PHP Version: 7.0.13-0ubuntu0.16.10.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `zaitona`
@@ -30,7 +36,6 @@ CREATE TABLE `courses` (
   `featured_image` varchar(255) DEFAULT NULL,
   `user_id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT '1',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
@@ -38,6 +43,13 @@ CREATE TABLE `courses` (
 -- Dumping data for table `courses`
 --
 
+INSERT INTO `courses` (`id`, `title`, `introduction`, `description`, `requirement`, `audience`, `featured_image`, `user_id`, `category_id`, `created_at`) VALUES
+(8, 'Java', 'this is the java course introduction', 'this is description for java course\r\nthis is description for java course\r\nthis is description for java course\r\nthis is description for java course\r\nthis is description for java course', 'java course requirements\r\njava course requirements\r\njava course requirements', 'targeted audience', '2017/03/20a03423d3.jpeg', 1, 5, '2017-03-10 10:21:15'),
+(15, 'Javascript', 'javascript Introduction', 'Javascript course description. Javascript course description.\r\nJavascript course description. Javascript course description.\r\nJavascript course description.\r\nJavascript course description.\r\nJavascript course description. Javascript course description.\r\nJavascript course description. Javascript course description.', '*basic knowledge of programming terminologies.', '*Any one who is ready to go', '2017/03/e32d766746.png', 1, 6, '2017-03-12 04:51:11'),
+(16, 'Bash script', 'Intro to course .', 'Shell script course description. Shell script course description.\r\nShell script course description. Shell script course description.\r\nShell script course description.\r\nShell script course description. Shell script course description.\r\nShell script course description. Shell script course description.\r\nShell script course description.\r\nShell script course description.\r\nShell script course description.', '* RHSA1 course.', '*anyone anywhere', '2017/03/14eb48467d.png', 1, 10, '2017-03-12 04:55:27'),
+(17, 'RHSA1', 'Intro RHSA1 course.', 'RHSA1 course description. RHSA1 course description.\r\nRHSA1 course description. RHSA1 course description.\r\nRHSA1 course description.\r\nRHSA1 course description. RHSA1 course description.\r\nRHSA1 course description. RHSA1 course description.\r\nRHSA1 course description. \r\nRHSA1 course description.\r\nRHSA1 course description. RHSA1 course description.\r\nRHSA1 course description.', '*..', '*anyone', '2017/03/d09b89b062.jpg', 1, 7, '2017-03-12 04:56:58'),
+(14, 'Php', 'This is php course.', 'Php course description. Php course description.\r\nPhp course description.\r\nPhp course description.\r\nPhp course description. Php course description.\r\nPhp course description. Php course description.\r\nPhp course description.\r\nPhp course description. Php course description.', '*programming background\r\n*awareness of client and server side terminologies', '*any body can do this', '2017/03/24c26f26d5.png', 1, 5, '2017-03-12 04:45:29'),
+(18, 'RHSA2', 'Intro to RHSA1 course.', 'RHSA2 course description.\r\nRHSA2 course description.\r\nRHSA2 course description. RHSA2 course description.\r\nRHSA2 course description. RHSA2 course description.\r\nRHSA2 course description. RHSA2 course description.\r\nRHSA2 course description.\r\nRHSA2 course description.', 'RHSA1 course .', '*Adminstration', '2017/03/18569699ac.jpg', 1, 7, '2017-03-12 04:58:14');
 
 --
 -- Indexes for dumped tables
@@ -47,7 +59,7 @@ CREATE TABLE `courses` (
 -- Indexes for table `courses`
 --
 ALTER TABLE `courses`
-ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -57,4 +69,7 @@ ADD PRIMARY KEY (`id`);
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
