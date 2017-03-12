15bc89d04c62d27e24743ea1838b2ea376bc288b
walid ahmed <walid_ahmed_elsayed@yahoo.com>
2017-03-12 14:42:24 +0200

    Adjust The Blog View Frontend

--- application/Modules/Frontend/Views/blog/view.php
+++ application/Modules/Frontend/Views/blog/view.php
@@ -1,5 +1,9 @@
-<div class="main container">
-<h3 class="ui header label"><?php echo $this->row['title']; ?></h3><br><br>
-<div class="description"> <?php echo $this->row['content']; ?></div><br>
+<div class="ui main container">
+
+<h2 class="ui dividing header"><?php echo $this->row['title']; ?></h2><br><br>
+
+<div class="description"> <?php echo wordwrap($this->row['content'], 200, "<br />\n"); ?></div><br>
+
 <h5><?php echo $this->row['created_at']; ?></h5>
+
 </div>
