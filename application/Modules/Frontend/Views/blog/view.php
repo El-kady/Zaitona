53b845a577f264dd5f52a7e4a52029fcc5a62ec0
walid ahmed <walid_ahmed_elsayed@yahoo.com>
2017-03-12 15:00:42 +0200

    Fix About Us Layout

--- application/Modules/Frontend/Views/blog/view.php
+++ application/Modules/Frontend/Views/blog/view.php
@@ -2,7 +2,7 @@
 
 <h2 class="ui dividing header"><?php echo $this->row['title']; ?></h2><br><br>
 
-<div class="description"> <?php echo wordwrap($this->row['content'], 200, "<br />\n"); ?></div><br>
+<div class="description"><h3> <?php echo wordwrap($this->row['content'], 200, "<br />\n"); ?></h3></div><br>
 
 <h5><?php echo $this->row['created_at']; ?></h5>
 
